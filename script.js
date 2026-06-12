07c5b576d05960315857a928896136377bf1bb97
rallebertan <rallebertan@gmail.com>
2026-06-12 13:35:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+// VM Tips Bygma 2026
 
+console.log("VM Tips Bygma laddad");
+
+// Här kommer senare:
+ // - Automatisk leaderboard
+ // - Matcher
+ // - Statistik
+ // - Import från Excel/JSON
+
+const leaderboard = [
+  { namn: "MNH", poang: 4 },
+  { namn: "RNI", poang: 3 },
+  { namn: "PTR", poang: 3 }
+];
+
+leaderboard.forEach(spelare => {
+  console.log(
+    `${spelare.namn}: ${spelare.poang} poäng`
+  );
+});
